f8c3eba68c7210047025b9fdfb260857182a63e3
Artur Ferfecki <artur.ferfecki@sikt.no>
2022-09-05 10:14:25 +0200

    dishes reset after all was cooked

--- app.py
+++ app.py
@@ -1,58 +1,80 @@
-import sqlite3
-import random
-import sys
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from config import Config
 
 app = Flask(__name__)
+# initialize config
+app.config.from_object(Config)
+# Initialize the database
+db = SQLAlchemy(app)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+# create db model
+class Dish(db.Model):
+    __tablename__ = 'dishes'
+    id = db.Column(db.Integer, primary_key=True)
+    dish_name = db.Column(db.String(50), nullable=False)
+    dish_type = db.Column(db.String(20), nullable=False)
+    cooked = db.Column(db.Boolean, nullable=False)
 
-def read_from_db(dish_type):
-    conn = get_db_connection()
-    query = 'SELECT * FROM dishes WHERE dish_type=? AND cooked=0'
-    dishes = conn.execute(query, [dish_type]).fetchall()
-    conn.close()
-    return dishes
+    # create a string
+    def __repr__(self):
+        return '<dish name: %s, dish type: %s, cooked: %s>' % (self.dish_name, self.dish_type, self.cooked)
 
-def write_to_db(dish_name, dish_type):
-    conn = get_db_connection()
-    query = 'INSERT INTO dishes (dish_name, cooked, dish_type) VALUES (?, ?, ?)'
-    conn.execute(query, (dish_name, 0, dish_type))
+# create a form class
+class NewDishForm(FlaskForm):
+    dish_name = StringField(' Name of dish you want to add', validators=[DataRequired()])
+    dish_type = StringField('Type of the dish', validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+def change_dish_status(dish_type):
+    db.session.query(Dish).filter(Dish.dish_type == dish_type).update({'cooked': 0})
+    db.session.commit()
     return
 
-def change_dish_status():
-    return
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    dishes = read_from_db('main')
+    dishes = db.session.query(Dish).filter(Dish.cooked == 0)
+    # reset dishes if all were cooked
+    dish_types = ['main', 'salad']
+    for dish_type in dish_types:
+        if not dishes.filter(Dish.dish_type == dish_type).first():
+            change_dish_status(dish_type)
+
+    if request.method == 'POST':
+        dish_type = request.form.get('name')
+        return redirect(url_for('pick_meal', dish_type=dish_type))
     return render_template('home.html', dishes=dishes)
 
-@app.route('/')
-def confirm():
-    change_dish_status()
-    dishes = read_from_db('main')
-    return render_template('home.html', dishes=dishes)
+@app.route('/<dish_id>')
+def confirm(dish_id):
+    db.session.query(Dish).filter(Dish.id == dish_id).update({'cooked': 1})
+    db.session.commit()
+    return redirect(url_for('home'))
 
-@app.route('/meal')
-def pick_meal():
-    dishes = read_from_db('main')
-    meal_choice = random.randint(1, len(dishes))
-    for k in dishes:
-        if k['id'] == meal_choice:
-            dish = k['dish_name']
-    return render_template('pick.html', dish=dish)
+@app.route('/pick_meal/<string:dish_type>', methods=['GET', 'POST'])
+def pick_meal(dish_type):
+    #pick random dish which was not cooked yet
+    dish = db.session.query(Dish).filter(Dish.dish_type == dish_type, Dish.cooked == 0).order_by(func.random()).first()
+    return render_template('pick_meal.html', dish=dish, dish_type=dish_type)
 
-@app.route('/add_dish')
+@app.route('/add_dish', methods=['GET', 'POST'])
 def add_dish():
-    return render_template('add_dish.html')
+    dish_name, dish_type = None, None
+    form = NewDishForm()
+    # validate form
+    if form.validate_on_submit():
+        dish_name = form.dish_name.data
+        dish_type = form.dish_type.data
+        form.dish_name.data, form.dish_type.data = '', ''
+        new_dish = Dish(dish_name=dish_name, dish_type=dish_type, cooked=0)
 
-@app.route('/confirmation', methods=['POST'])
-def confirmation():
-    dish_name = request.form.get("dish_name")
-    dish_type = request.form.get("dish_type")
-    write_to_db(dish_name, dish_type)
-    return render_template('confirmation.html', dish_name=dish_name)
+        #push to database
+        db.session.add(new_dish)
+        db.session.commit()
+        return redirect('/add_dish')
+    else:
+        return render_template('add_dish.html', dish_name=dish_name, dish_type=dish_type, form=form)
